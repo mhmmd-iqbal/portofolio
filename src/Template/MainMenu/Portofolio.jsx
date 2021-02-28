c3d295e5b86ef0b14614e43de443aac98767e42f
mhmmd-iqbal <mhmmd.iqbal.june@gmail.com>
2021-02-28 14:00:19 +0700

    update

--- src/Template/MainMenu/Portofolio.jsx
+++ src/Template/MainMenu/Portofolio.jsx
@@ -22,12 +22,12 @@ class Portofolio extends Component {
 						<img src={this.props.data.image} className="img-res" alt="" />
 						<div className="portfolio-item-info">
 							<h4>{this.props.data.name}</h4>
-							<a data-toggle="modal" onClick={this.openModal}>
+							{/* <a data-toggle="modal" onClick={this.openModal}>
 								<span className="glyphicon glyphicon-eye-open"></span>
 							</a>
 							<a href="#">
 								<span className="glyphicon glyphicon-link"></span>
-							</a>
+							</a> */}
 						</div>
 					</div>
 				</div>
